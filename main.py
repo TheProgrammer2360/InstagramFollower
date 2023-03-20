e3825d70756bad7c3f010981614b9ff769a6e453
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-03-20 03:55:53 +0200

    removed the print line for the value of password and phone number

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ import os
 mybot = Instagram()
 phone_number = os.environ["PHONENUMBER"]
 password = os.environ["PASSWORD"]
-print(f"phone number : {phone_number}\n password: {password}")
+
 time.sleep(3600)
