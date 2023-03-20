5435bc0bc5548f3cf73596d5dd55aa6ffa34120c
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-03-20 03:19:16 +0200

    create the bot.py and added chromedriver.exe, on the bot.py added the main structure of the Instagram class

---
+++ bot.py
@@ -0,0 +1,25 @@
+from selenium import  webdriver
+
+
+class Instagram:
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path="/chromedriver.exe")
+        self.driver.get(url="https://www.instagram.com")
+        self.is_logged_in = False
+        self.is_in_user_profile = False
+        self.username = ""
+        self.password = ""
+        self.on_followers = False
+
+    def login_with_facebook(self, username: str, password: str) -> bool:
+        """Will try to log in, if successful return true else false"""
+        pass
+
+    def go_to_user_url(self, url: str) -> None:
+        """makes sure that the user is signed in or else throw an exception"""
+        pass
+
+    def go_to_followers(self) -> None:
+        """requires the user to be signed in and with to be a user profile"""
+        pass
+
