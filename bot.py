5bd02c20eaf4dd41cfb9be1b69ea52088147ffb8
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-03-20 04:23:23 +0200

    implemented the login with facebook method but still incomplete (It does login in )

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from selenium import  webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 class Instagram:
     def __init__(self):
@@ -8,13 +10,35 @@ class Instagram:
         self.driver.get(url="https://www.instagram.com")
         self.is_logged_in = False
         self.is_in_user_profile = False
-        self.username = ""
-        self.password = ""
         self.on_followers = False
 
     def login_with_facebook(self, username: str, password: str) -> bool:
         """Will try to log in, if successful return true else false"""
-        pass
+        xpath = "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/" \
+                "article/div[2]/div[1]/div[2]/form/div/div[5]/button"
+        try:
+            # wait ten seconds for the login with facebook button to show and then click it
+            WebDriverWait(self.driver, 10).until(
+                ec.presence_of_element_located((By.XPATH, xpath))
+            )
+        finally:
+            # when the login in with facebook button shows up just get hold of it
+            login_with_facebook = self.driver.find_element(By.XPATH, xpath)
+            # and click it
+            login_with_facebook.click()
+
+        # getting hold of the email input area
+        email_input_area = self.driver.find_element(By.ID, "email")
+        # input the username
+        email_input_area.send_keys(username)
+        # Getting hold of the password input area
+        password_input_area = self.driver.find_element(By.ID, "pass")
+        # # input the password
+        password_input_area.send_keys(password)
+        # getting hold of the login button
+        login_button = self.driver.find_element(By.ID, "loginbutton")
+        login_button.click()
+
 
     def go_to_user_url(self, url: str) -> None:
         """makes sure that the user is signed in or else throw an exception"""
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from bot import Instagram
 import time
 import os
 
-
 mybot = Instagram()
 phone_number = os.environ["PHONENUMBER"]
 password = os.environ["PASSWORD"]
 
+mybot.login_with_facebook(password=password, username=phone_number)
 time.sleep(3600)
